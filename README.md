18b03c05d1e8419a1575a23d103fbef4caad0074
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-12-03 12:42:12 -0500

    Fixed bugs.

---
+++ README.md
@@ -0,0 +1,8 @@
+# FFT
+
+Fast Fourier Transformer
+
+Should work fine now.
+
+ffttest.f95 contains the code for testing, the others contain actual subroutines for computing the fast fourier transform.
+
--- fast_fourier_transformR.f95
+++ fast_fourier_transformR.f95
@@ -1,5 +1,5 @@
 recursive subroutine fast_fourier_transformR(auxre, auxim, re, im, sz, gp, off)
-    implicit none
+implicit none
     real, dimension(:), allocatable::auxre
     real, dimension(:), allocatable::auxim
     real, dimension(:), allocatable::re
@@ -8,24 +8,30 @@ recursive subroutine fast_fourier_transformR(auxre, auxim, re, im, sz, gp, off)
     integer::gp
     integer::off
     integer::i
+    integer::odd
     real::tmpre
     real::tmpim
     real::tau
-    tau = 3.1415926535897932 * 2
+    real::rt_of_unity_re
+    real::rt_of_unity_im
+    tau = 6.2831853071795865
     if(sz > gp)then
         call fast_fourier_transformR(auxre, auxim, re, im, sz, gp * 2, off)
         call fast_fourier_transformR(auxre, auxim, re, im, sz, gp * 2, off + gp)
         do i=1,sz/2,gp
-            auxre(i + off) = re(2 * i - 1 + off)
-            auxim(i + off) = im(2 * i - 1 + off)
-            tmpre = re(2 * i - 1 + off + gp) * cos(tau * i / gp / sz) + im(2 * i - 1 + off + gp) * sin(tau * i / gp / sz)
-            tmpim = im(2 * i - 1 + off + gp) * cos(tau * i / gp / sz) - re(2 * i - 1 + off + gp) * sin(tau * i / gp / sz)
-            auxre(i + off) = auxre(i + off) + tmpre
-            auxim(i + off) = auxim(i + off) + tmpim
+            auxre(i + off) = re(1 + 2 * (i - 1) * gp + off)
+            auxim(i + off) = im(1 + 2 * (i - 1) * gp + off)
+            rt_of_unity_re = cos(tau * (i - 1) * gp / sz)
+            rt_of_unity_im = sin(tau * (i - 1) * gp / sz)
+            odd = (2 * i - 1) * gp + off + 1
+            tmpre = re(odd) * rt_of_unity_re + im(odd) * rt_of_unity_im
+            tmpim = im(odd) * rt_of_unity_re - re(odd) * rt_of_unity_im
             auxre(sz / 2 + i + off) = auxre(i + off) - tmpre
             auxim(sz / 2 + i + off) = auxim(i + off) - tmpim
+            auxre(i + off) = auxre(i + off) + tmpre
+            auxim(i + off) = auxim(i + off) + tmpim
         enddo
-        do i=1,sz,gp
+        do i=off + 1,sz,gp
             re(i) = auxre(i)
             im(i) = auxim(i)
         enddo
